01b826cfa7937128f74c2f9e7b3cf54f5bbda740
Louie O'Grady <l.ogrady@gmail.com>
2019-02-06 11:03:40 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  if block_given?
+    array2 = []
+    i = 0 
+    while i < array.length 
+    if yield(array[i]) == true 
+      array2 << array[i]
+    end 
+    i+=1 
+  end
+  array2
+  else 
+    return "Hey! No block was given!"
+    end
 end
